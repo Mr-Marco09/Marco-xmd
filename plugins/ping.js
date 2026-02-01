8fa06f5187a0ed18d8a0d4192688e59ef0857169
Dark-SHADOW-OFFICIAL <221742868+Mr-Marco09@users.noreply.github.com>
2026-02-01 18:29:27 -0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,10 +1,22 @@
-//////plugins/ping.js//////////
+///////plugins/ping.js/////
+const config = require("../config.json");
 
 module.exports = {
-    name: "ping", // Le nom de la commande
+    name: "ping",
     description: "Vérifie la latence du bot",
-    async execute(marco, msg, args) {
-        const from = msg.key.remoteJid;
-        await marco.sendMessage(from, { text: "Pong! 🏓" });
+    async execute(conn, m, args) {
+        const from = m.key.remoteJid;
+        
+        // Calcul de la latence
+        const start = Date.now();
+        const firstMsg = await conn.sendMessage(from, { text: "Calcul en cours..." }, { quoted: m });
+        const end = Date.now();
+        const latency = end - start;
+
+        // Mise à jour du message avec le temps de réponse
+        await conn.sendMessage(from, { 
+            text: `*🏓 Pong !*\n\n🚀 Vitesse : *${latency}ms*\n🤖 Bot : *${config.botName}*`,
+            edit: firstMsg.key 
+        });
     }
 };
